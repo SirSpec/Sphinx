539280b341fcc0174a78169636cf52bd3c0f6865
Spec <dawid.spec.jachowicz@gmail.com>
2020-10-14 14:05:47 +0200

    Add main redux components - rootReducer and store - App.jsx and index.js

---
+++ src/client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ActivityLogContainer from "./components/ActivityLog/ActivityLogContainer";
+
+import "./styles/global";
+
+const App = () => {
+  return (
+    <ActivityLogContainer />
+  );
+};
+
+export default App;
