e8e2501b69f5a48d73e140202d45abbabc3bf2b1
Spec <dawid.spec.jachowicz@gmail.com>
2020-10-19 13:18:42 +0200

    Change Sudoku component implementation - Use ul HTML tag - Change styling - Remove Region reference - Add SignalR connection

--- src/client/src/components/sudoku/Sudoku.jsx
+++ src/client/src/components/sudoku/Sudoku.jsx
@@ -1,27 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from "prop-types";
+import * as signalR from "@microsoft/signalr"
 
 import styles from "./sudoku"
 
-import Region from "../region/Region"
 import Cell from "../cell/Cell"
 
-const Sudoku = ({ cells }) => {
+const Sudoku = ({ sudoku, setSudoku, changeCell }) => {
+    const [connection, setConnection] = useState(null);
+
+    useEffect(() => {
+        var connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:5001/hub").build()
+
+        connection.on("ReceiveMap", sudoku => {
+            console.log(sudoku)
+            setSudoku(sudoku);
+        })
+
+        connection.start().then(() => {
+            console.log("Map component connection started.")
+            connection.invoke("SendMap").catch(err => {
+                console.log(err.toString());
+            });
+        }).catch(err => {
+            console.log(err)
+        });
+
+        setConnection(connection);
+    }, []);
+
     return (
-        <div className={styles.sudoku}>
-            {cells.map((row, index) =>
-                <Region key={index}>
-                    {row.map(cell =>
-                        <Cell key={cell.id} value={cell.value} />
-                    )}
-                </Region>
-            )}
-        </div>
+        <ul className={styles.sudoku}>
+            {sudoku.cells ? sudoku.cells.map((row, rowIndex) =>
+                row.map((cell, columnIndex) =>
+                    <Cell
+                        key={`${rowIndex}_${columnIndex}`}
+                        cell={
+                            {
+                                value: cell,
+                                row: rowIndex,
+                                column: columnIndex
+                            }
+                        }
+                        onValueChange={changeCell} />
+                )
+            ) : "Loading"}
+        </ul>
     );
 };
 
 Sudoku.propTypes = {
-    cells: PropTypes.arrayOf(PropTypes.array.isRequired).isRequired
+    sudoku: PropTypes.any.isRequired
 };
 
 export default Sudoku;
