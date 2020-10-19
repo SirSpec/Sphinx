45b14518856972be71271a0bd98d18a1aafed3f5
Spec <dawidjachowicz91@gmail.com>
2020-10-19 13:21:36 +0200

    Replace log reducer with Sudoku - Remove log component with reducer - Add sudoku reducer - Add SudokuContainer - Adjust App component

--- src/client/src/actionCreators/logsActionCreators.js
+++
@@ -1,8 +0,0 @@
-import LogsActions from "../actions/logsActions"
-
-export function addLog(text) {
-    return {
-        type: LogsActions.ADD_LOG,
-        text
-    }
-}
---
+++ src/client/src/actions/sudokuActions.js
@@ -0,0 +1,4 @@
+export default {
+    SET_SUDOKU: "SET_SUDOKU",
+    CHANGE_CELL: "CHANGE_CELL"
+}
