357e7414d1325885dc1bec8ba3df6f9038edbfe1
Spec <dawid.spec.jachowicz@gmail.com>
2020-10-19 13:28:46 +0200

    Add WebApi initial implementation

---
+++ src/server/Infrastructure/WebApi/Hubs/MapHub.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Sphinx.Infrastructure.WebApi.Hubs
+{
+    public class Sudoku
+    {
+        public int[][] Cells => new int[][]
+        {
+            new int[]{1,2,0,4,5,6,7,8,9},
+            new int[]{1,2,3,4,5,6,7,8,9},
+            new int[]{1,2,3,4,5,6,7,8,9},
+            new int[]{1,2,3,4,5,6,7,8,9},
+            new int[]{1,2,3,4,5,6,7,8,9},
+            new int[]{1,2,3,4,5,6,7,8,9},
+            new int[]{1,2,3,4,5,6,7,8,9},
+            new int[]{1,2,3,4,5,6,7,8,9},
+            new int[]{1,2,3,4,5,6,7,8,9},
+        };
+    }
+
+    public class MapHub : Hub
+    {
+        public async Task SendMap()
+        {
+            await Clients.All.SendAsync("ReceiveMap", new Sudoku());
+            // , new JsonSerializerOptions()
+            // {
+            //     PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            // }));
+        }
+    }
+}
