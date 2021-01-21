084788eb45ff295a49f20c045d0c4dd0680eb87a
Spec <dawid.spec.jachowicz@gmail.com>
2021-01-21 21:20:24 +0100

    Implement SudokuSolver and refactor MapHub

--- src/server/Infrastructure/WebApi/Hubs/MapHub.cs
+++ src/server/Infrastructure/WebApi/Hubs/MapHub.cs
@@ -1,37 +1,17 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json;
+using Sphinx.Domain.Sudoku;
 using System.Threading.Tasks;
 
 namespace Sphinx.Infrastructure.WebApi.Hubs
 {
-    public class Sudoku
-    {
-        public int[][] Cells => new int[][]
-        {
-            new int[]{1,2,0,4,5,6,7,8,9},
-            new int[]{1,2,3,4,5,6,7,8,9},
-            new int[]{1,2,3,4,5,6,7,8,9},
-            new int[]{1,2,3,4,5,6,7,8,9},
-            new int[]{1,2,3,4,5,6,7,8,9},
-            new int[]{1,2,3,4,5,6,7,8,9},
-            new int[]{1,2,3,4,5,6,7,8,9},
-            new int[]{1,2,3,4,5,6,7,8,9},
-            new int[]{1,2,3,4,5,6,7,8,9},
-        };
-    }
-
     public class MapHub : Hub
     {
-        public async Task SendMap()
+        public async Task Solve(int[][] board)
         {
-            await Clients.All.SendAsync("ReceiveMap", new Sudoku());
-            // , new JsonSerializerOptions()
-            // {
-            //     PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-            // }));
+            var sudokuSolver = new SudokuSolver(board);
+            sudokuSolver.TryToSolve();
+
+            await Clients.All.SendAsync("ReceiveSudoku", sudokuSolver.Squares);
         }
     }
 }
