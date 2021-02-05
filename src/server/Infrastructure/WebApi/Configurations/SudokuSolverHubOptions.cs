db23d09df1685482c32d53381d68818f78df9aab
Spec <dawid.spec.jachowicz@gmail.com>
2021-02-05 09:01:20 +0100

    Refactor Infrastructure - Extract SudokuSolverHubOptions - Fix naming and format

---
+++ src/server/Infrastructure/WebApi/Configurations/SudokuSolverHubOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Sphinx.Infrastructure.WebApi.Configurations
+{
+    public class SudokuSolverHubOptions
+    {
+        public const string SudokuSolverHub = "SudokuSolverHub";
+        
+        public string? Url { get; set; }
+    }
+}
